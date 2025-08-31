892911d6710a64f9161c31eb0e85c7d0fba77692
Saran-2205 <srinivasansaranjayan@gmail.com>
2025-09-01 01:42:41 +0530

    aw

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    outDir: 'dist',     // Ensure this matches your deployment folder
+    emptyOutDir: true,  // Optional: clears old files before build
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Adjust this to your backend server URL
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: true,
       }
     }
-  },
+  }
 })
