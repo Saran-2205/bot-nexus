015d260ee71935f0c0bdc08f7cdce11b5d68ee4e
Saran-2205 <srinivasansaranjayan@gmail.com>
2025-09-01 01:02:08 +0530

    sds

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,19 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
+const allowedOrigins = ['https://www.botnexus.in','https://bot-nexus-6qox.onrender.com'];
+
 app.use(cors({
-  origin: "https://www.botnexus.in/",
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
