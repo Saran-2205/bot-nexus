47ae62101141a61ec3bfad332d42ab0bd1ea2357
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-09-03 09:57:27 +0000

    sdf

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken';
 
-const generateTokenAndSetCookie = (userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d"
+const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
     });
 
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000, //Milliseconds
-        httpOnly:true,
-        secure:true, //prevent XXS attack cross site scripting attacks
-        sameSite:"None", //CSRF attacks
-        secure:process.env.NODE_ENV!=="development" 
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //Milliseconds
+        httpOnly: true,
+        secure: true, //prevent XXS attack cross site scripting attacks
+        sameSite: "None", //CSRF attacks
     })
+
+    console.log("Cookies:", req.cookies);
+    console.log("JWT:", req.cookies.jwt);
+
 };
 
 export default generateTokenAndSetCookie;
