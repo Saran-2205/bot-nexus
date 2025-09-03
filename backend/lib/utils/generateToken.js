cecf34e85cfc6db780b8da21a89583918077ff3f
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-09-03 09:36:36 +0000

    sd

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000, //Milliseconds
         httpOnly:true, //prevent XXS attack cross site scripting attacks
-        sameSite:"None", //CSRF attacks
+        sameSite:"Strict", //CSRF attacks
         secure:process.env.NODE_ENV!=="development" 
     })
 };
