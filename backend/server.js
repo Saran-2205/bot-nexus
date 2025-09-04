8d86f58b182f4a39eb72bb65eba3c7f14c80a71e
Saran Jayan <srinivasansaranjayan@gmail.com>
2025-09-04 16:49:36 +0000

    sadsd

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,6 @@ const allowedOrigins = ['https://www.botnexus.in', 'https://bot-nexus-6qox.onren
 app.use(cors({
   origin: function (origin, callback) {
     console.log("Incoming origin:", origin);
-    if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
